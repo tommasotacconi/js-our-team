b8fed06b16a76cce9de7b42db1ad6c1605b75399
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-28 15:34:22 +0200

    data and imgs set up

--- jscript/script.js
+++ jscript/script.js
@@ -1,1 +1,12 @@
-console.log('ok js')
+// DATI DI PARTENZA
+/* Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg  */
+
+
+// FASE DI PREPARAZIONE 
+// 1. Creo l'array di oggetti con i dati soprastanti. Ogni persona
+// costituisce un oggetto dell'array
