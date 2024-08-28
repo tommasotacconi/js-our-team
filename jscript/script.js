cb815a2ae81fec4140ad9372386338ac9d2835e1
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-28 15:53:11 +0200

    set up phase

--- jscript/script.js
+++ jscript/script.js
@@ -10,3 +10,39 @@ Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg  */
 // FASE DI PREPARAZIONE 
 // 1. Creo l'array di oggetti con i dati soprastanti. Ogni persona
 // costituisce un oggetto dell'array
+const team = [
+  {
+  completeName: 'Wayne Barnett',
+  position: 'Founder & CEO',
+  photo: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    completeName: 'Angela Caroll',
+    position: 'Chief Editor',
+    photo: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+  completeName: 'Walter Gordon',
+  position: 'Office Manager',
+  photo: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    completeName: 'Angela Lopez',
+    position: 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    completeName: 'Scott Estrada',
+    position: 'Developer',
+    photo: 'scott-estrada-developer.jpg',
+  },
+  {
+    completeName: 'Barbara Ramos',
+    position: 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jpg',
+  }
+];
+
+console.log(team);
+
+
